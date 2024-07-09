227f447552e2c0eb43dea7e59145ea20ce6feee8
victorsp <victorspessoa@gmail.com>
2024-07-09 18:35:08 -0300

    commit inicial
    commit inicial

---
+++ codespace/src/main/java/com/example/codespace/CodespaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.codespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodespaceApplication.class, args);
+	}
+
+}
---
+++ codespace/src/test/java/com/example/codespace/CodespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
